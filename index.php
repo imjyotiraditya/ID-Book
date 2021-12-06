62a6bff5ba4ce38fa62b3ee5075fdf2c01a1026c
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-12-07 01:16:25 +0530

    Update homepage msg
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $title = "Home Page"; // set page title
 include("inc/header.php"); // include the header file
 ?>
 <h1>Welcome</h1>
-<p>Welcome dear guset, please <a href="login.php">login</a> to view or update your profile. If you don't have an account
+<p>Welcome dear guest, please <a href="login.php">login</a> to view or update your profile. If you don't have an account
     please <a href="register.php">register</a> here. If you want view someone else's profile, please scan the QR Code of
     the user.</p>
 <?php
