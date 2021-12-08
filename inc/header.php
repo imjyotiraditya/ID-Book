fda5db3f269458c6a243d05a57d838b6e0ab2238
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-12-08 13:00:19 +0530

    Add favicon
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- inc/header.php
+++ inc/header.php
@@ -12,6 +12,7 @@ include("db.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
     <link rel="stylesheet" href="css/styles.css" />
+    <link rel="shortcut icon" type="image/jpg" href="photos/favicon.ico"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $title; ?></title>
 </head>
@@ -20,11 +21,7 @@ include("db.php");
     <header class="text-gray-400 bg-gray-900 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
-                    stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                    viewBox="0 0 24 24">
-                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                </svg>
+                <img src="photos/favicon.ico" alt="logo" class="w-10 h-10">
                 <span class="ml-3 text-xl">ID Book</span>
             </a>
             <nav
