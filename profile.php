63383df8de660a8fa0bad17e1e9ac39014d68829
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-12-10 00:08:38 +0530

    Update QR Code image URL.
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- profile.php
+++ profile.php
@@ -80,7 +80,7 @@ if (!checkLogin()) {
     </div>
     <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
         <img class="lg:w-1/6 md:w-3/6 w-4/6 mb-10 object-cover object-center rounded" alt="hero"
-            src="qr_codes/qr_<?= $data[0]['id']; ?>.png">
+            src="assets/img/qr_imgs/qr_<?= $data[0]['id']; ?>.png">
         <div class="text-center lg:w-2/3 w-full">
             <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">QR Code</h1>
             <p class="leading-relaxed mb-8">Scan to visit the profile</p>
