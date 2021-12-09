7809b39baf17bbae42fbd2485c422b92144d48e6
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-12-10 00:00:41 +0530

    Define body color
    hax to fix white spaces after making footer stay at bottom
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@ echo $title;
 ?></title>
 </head>
 
-<body>
+<body class="bg-gray-900">
     <header class="text-gray-400 bg-gray-900 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
