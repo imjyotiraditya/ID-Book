116db21a185806757f2f57474875d7b41bfeccdb
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-12-10 18:38:12 +0530

    Add github link
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- includes/header.php
+++ includes/header.php
@@ -20,13 +20,7 @@ echo $title;
 <body class="bg-gray-900">
     <header class="text-gray-400 bg-gray-900 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-                <img src="assets/img/favicon.ico" alt="logo" class="w-10 h-10">
-                <span class="ml-3 text-xl">ID Book</span>
-            </a>
-            <nav
-                class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-                <a href="/" class="mr-5 hover:text-white">Home</a>
+            <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
                 <?php
 if (!checkLogin()) {
 ?>
@@ -41,6 +35,27 @@ if (!checkLogin()) {
 }
 ?>
             </nav>
+            <a href="/"
+                class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
+                <img src="assets/img/favicon.ico" alt="logo" class="w-10 h-10">
+                <span class="ml-3 text-xl xl:block lg:hidden">ID Book</span>
+            </a>
+            <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+                <a href="https://github.com/imjyotiraditya/ID-Book">
+                    <button
+                        class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+                        <svg width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                            <path
+                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                        </svg>
+                        <span class="ml-1 text-s xl:block lg:hidden">Source</span>
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </button>
+                </a>
+            </div>
         </div>
     </header>
     <main>
